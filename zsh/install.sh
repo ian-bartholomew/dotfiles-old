c386a6677743222c3a6b4acd8dab22dd278ebedd
ian-bartholomew <ibartholomew@gmail.com>
2017-04-01 22:11:11 -0700

    fixing oh my zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,10 +5,11 @@
 # This installs oh-my-zsh
 
 # Check for oh-my-zsh
-if [ ! -d "~/.oh-my-zsh" ]; then
+DIRECTORY="$HOME/.oh-my-zsh"
+if [ ! -d "${DIRECTORY}" ]; 
 then
   echo "  Installing oh-my-zsh for you."
-  curl -L http://install.ohmyz.sh | sh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 exit 0
