3bca6bf679d93d660be824c256ce8917c8a5a016
ian-bartholomew <ibartholomew@gmail.com>
2021-03-17 22:11:31 -0700

    updating font install script

--- fonts/install.sh
+++ fonts/install.sh
@@ -6,18 +6,27 @@ echo 'Installing powerline fonts'
 
 GIT=`which git`
 
-# make our temp directory 
+# make our temp directory
 if [ ! -d ~/tmp ]; then
-  mkdir ~/tmp 
-fi 
+  mkdir ~/tmp
+fi
 
 cd ~/tmp
 
+# powerline fonts
 $GIT clone https://github.com/powerline/fonts.git
 cd fonts
 
 # Install
 ./install.sh
+
+# Nerdfonts
+cd ..
+$GIT clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh Hack
+./install.sh UbuntuMono
+./install.sh Inconsolata
 
 # Cleanup
 cd ../..
